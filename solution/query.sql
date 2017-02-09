d686ec55f45b9248ea3ba14a5189349af687541a
student <michal.niedziela@labnoratory.com>
2017-02-09 15:49:19 +0100

    test task4

--- solution/query.sql
+++ solution/query.sql
@@ -1,1 +1,8 @@
 -- Write your solution here
+
+CREATE SEQUENCE email_id START 1;
+ALTER TABle users
+ADD COLUMN email TEXT DEFAULT 'test' || nextval('email_id') || '@test.com' NOT NULL UNIQUE;
+
+
+
