bcde61031c07253c5383c4c212edab97eda09a3f
student <michal.niedziela@labnoratory.com>
2017-02-10 12:46:02 +0100

    test task6

--- solution/daily_sum_of_orders.sql
+++ solution/daily_sum_of_orders.sql
@@ -1,1 +1,17 @@
 -- Write your solution here
+
+
+CREATE OR REPLACE FUNCTION update_daily_trigger_function()
+RETURNS trigger AS
+$$
+    BEGIN
+       REFRESH MATERIALIZED VIEW daily_sum_of_orders;
+    RETURN NULL;
+    END;
+$$ LANGUAGE PLPGSQL;
+
+CREATE TRIGGER update_daily_trigger
+AFTER INSERT OR UPDATE OR DELETE
+    ON orders
+        FOR EACH STATEMENT
+            EXECUTE PROCEDURE update_daily_trigger_function();
--- solution/top_100_buyers.sql
+++ solution/top_100_buyers.sql
@@ -1,1 +1,22 @@
 -- Write your solution here
+CREATE OR REPLACE FUNCTION update_top_100_trigger_function()
+RETURNS trigger AS
+$$
+    BEGIN
+       REFRESH MATERIALIZED VIEW top_100_buyers;
+    RETURN NULL;
+    END;
+$$ LANGUAGE PLPGSQL;
+
+CREATE TRIGGER test_100_trigger
+AFTER INSERT OR UPDATE OR DELETE
+    ON customer
+        FOR EACH ROW
+            EXECUTE PROCEDURE update_top_100_trigger_function();
+
+
+CREATE TRIGGER test_100_product_trigger
+AFTER INSERT OR UPDATE OR DELETE
+    ON product
+        FOR EACH ROW
+            EXECUTE PROCEDURE update_top_100_trigger_function();
