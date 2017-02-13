0d49c475dc495945de980f4fbf07dee2ab71f6a3
student <michal.niedziela@labnoratory.com>
2017-02-13 12:58:05 +0100

    test task8

--- solution/query.sql
+++ solution/query.sql
@@ -1,2 +1,5 @@
 -- Write your solution here
 -- For tests assertion purpose please cast breeds json field into text data type.
+select origin, count(origin) as count, (
+    json_agg(json_build_object( 'name',(SELECT r.name FROM (SELECT name) r), 'family', (SELECT r.family FROM (SELECT family) r) ) )
+  )::TEXT AS breeds from breed  group by origin order by origin ;
